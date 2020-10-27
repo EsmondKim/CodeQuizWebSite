de20b0a2a50959948b3838b58329f3c596218910
EsmondKim <ekelectronicmail@gmail.com>
2020-10-26 23:00:58 -0500

    Added next question functionality and game over screen.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CodeQuizWebSite
+
+All of the comments in the code are my own, inserted to document pseudocoding the rationale for the code.
+
+This repository was created for education purposes, and therefore, fair use is claimed regarding the use of any content within the repository, including words, images, and references to any copyrighted works.
--- testscript.js
+++ testscript.js
@@ -13,16 +13,27 @@ let question9El = document.querySelector("#question9");
 let question10El = document.querySelector("#question10");
 let correctAnswerEl = document.querySelector("#correctAnswer");
 let wrongAnswerEl = document.querySelector("#wrongAnswer");
+let kickAssEl = document.querySelector("#kickAss");
+let builtForThisEl = document.querySelector("#builtForThis");
 
+let gameOverEl = document.querySelector("#gameOver");
 let minutesDisplay = document.querySelector("#minutes");
 let secondsDisplay = document.querySelector("#seconds");
 
-let totalSeconds = 120;
+//Creating an array to use in next question function.
+let nextQuestion = [question1, question2];
+let nextQuestionOperate = 0;
+let nextQuestionIndex;
+//Setting the initial time to start the game.
+let totalSeconds = 4;
 
 function beginKumite() {
     console.log("beginKumite");
-    question1.classList.remove("hideQuestion");
-    question1.classList.add("showQuestion");
+    console.log(nextQuestionOperate);
+    nextQuestionIndex = nextQuestion[nextQuestionOperate];
+    console.log(nextQuestionIndex);
+    nextQuestionIndex.classList.remove("hideQuestion");
+    nextQuestionIndex.classList.add("showQuestion");
     beginButton.classList.add("hideButton");
     startTimer();
 }
@@ -32,10 +43,11 @@ function startTimer() {
     totalSeconds--;
     if (totalSeconds === 0) {
         clearInterval(interval);    
+        gameOverFunc();
      }
      renderTime();
     }, 1000);
-}  
+}   
 
 function renderTime() {
 minutesDisplay.textContent = getFormattedMinutes();
@@ -60,7 +72,7 @@ function getFormattedSeconds() {
         formattedSeconds = ":0" + (totalSeconds) % 60;
     }
     
-    if (totalSeconds < 10) {
+    else if (totalSeconds < 10) {
         formattedSeconds = ":0" + (totalSeconds) % 60;
     }
 
@@ -68,29 +80,66 @@ function getFormattedSeconds() {
 }
 
 //Correct answer function, add time to the clock if correct, deduct if incorrect.
-function correctAnswerSubmit() {   
-    correctAnswer.classList.remove("hideCorrect");
-    correctAnswer.classList.add("showCorrect");
-    question1.classList.remove("showQuestion");
-    question1.classList.add("hideQuestion");    
-} 
-
-function incorrectAnswerSubmit() {
-    wrongAnswer.classList.remove("hideWrong");
-    wrongAnswer.classList.add("showWrong");
-    question1.classList.remove("showQuestion");
-    question1.classList.add("hideQuestion");    
-}
-
 //Show next question function.
     //Here, consider an array with question1-10, and i increments each time a right or wrong answer screem is clicked.
+    
+    function correctAnswerSubmit() {   
+        nextQuestionIndex.classList.remove("showQuestion");
+        nextQuestionIndex.classList.add("hideQuestion");
+        correctAnswer.classList.remove("hideCorrect");
+        correctAnswer.classList.add("showCorrect");
+    } 
+    
+    function incorrectAnswerSubmit() {
+        nextQuestionIndex.classList.remove("showQuestion");
+        nextQuestionIndex.classList.add("hideQuestion");
+        wrongAnswer.classList.remove("hideWrong");
+        wrongAnswer.classList.add("showWrong");
+    }
 
+    function kickMoreAss() {
+        nextQuestionOperate++;
+        console.log(nextQuestionOperate);
+        nextQuestionIndex = nextQuestion[nextQuestionOperate];
+        console.log(nextQuestionIndex);
+        nextQuestionIndex.classList.remove("hideQuestion");
+        nextQuestionIndex.classList.add("showQuestion");
+        correctAnswer.classList.remove("showCorrect");
+        correctAnswer.classList.add("hideCorrect");
+        totalSeconds = totalSeconds + 5;     
+    }
+
+    function moreBuiltForThis() {
+        nextQuestionOperate++;
+        console.log(nextQuestionOperate);
+        nextQuestionIndex = nextQuestion[nextQuestionOperate];
+        console.log(nextQuestionIndex);
+        nextQuestionIndex.classList.remove("hideQuestion");
+        nextQuestionIndex.classList.add("showQuestion");
+        wrongAnswer.classList.remove("showWrong");
+        wrongAnswer.classList.add("hideWrong");  
+        totalSeconds = totalSeconds - 5;   
+    }
 //End the quiz function.
-    //Quiz ends when timere hits 0,
-    //or player wins if they complete question 10 with time left.
+//Quiz ends when timere hits 0, 
+    function gameOverFunc() {
+    console.log("game over");
+    nextQuestionIndex.classList.remove("showQuestion");
+    nextQuestionIndex.classList.add("hideQuestion");
+    gameOverEl.classList.remove("hideGameOver");
+    gameOverEl.classList.add("showGameOver");
+}
+
+//Or player wins if they complete question 10 with time left.  
+//Embed a specific question10 id to the correct answer to trigger the win screen.
+//Wrong answer for question10 get a "try again" screen for question 10.
+
 
 //High score from local storage function.
+
 
 correctChoiceEl.addEventListener("click", correctAnswerSubmit);
 incorrectChoiceEl.addEventListener("click", incorrectAnswerSubmit);
 beginButtonEl.addEventListener("click", beginKumite);
+kickAssEl.addEventListener("click", kickMoreAss);
+builtForThisEl.addEventListener("click", moreBuiltForThis);
