295f9f73adb47b41389f930c97b4f47f5b0893e5
EsmondKim <ekelectronicmail@gmail.com>
2020-10-28 13:54:35 -0500

    Got .textContent method working to print top score and initials from local storage to high score page.

--- highscorescript.js
+++ highscorescript.js
@@ -4,7 +4,14 @@ let numberOneScoreEl = document.querySelector("#numberOneScore");
 let numberTwoScoreEl = document.querySelector("#numberTwoScore");
 let numberThreeScoreEl = document.querySelector("#numberThreeScore");
 
-numberOneScoreEl.textContent = localStorage.getItem("highScore" "topInitials");
-numberTwoScoreEl.textContent = localStorage.getItem("secondScore" "secondInitials");
-numberThreeScoreEl.textContent = localStorage.getItem("thirdScore" "thirdInitials");
+let numberOnePlayerEl = document.querySelector("#numberOnePlayer");
+let numberTwoPlayerEl = document.querySelector("#numberTwoPlayer");
+let numberThreePlayerEl = document.querySelector("#numberThreePlayer");
 
+numberOneScoreEl.textContent = localStorage.getItem("highScore");
+numberTwoScoreEl.textContent = localStorage.getItem("secondScore");
+numberThreeScoreEl.textContent = localStorage.getItem("thirdScore");
+
+numberOnePlayerEl.textContent = localStorage.getItem("topInitials");
+numberTwoPlayerEl.textContent = localStorage.getItem("secondInitials");
+numberThreePlayerEl.textContent = localStorage.getItem("thirdInitials");
