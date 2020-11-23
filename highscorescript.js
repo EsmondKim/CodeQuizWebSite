92c7c9bcafc462a45be2dbded8ecae31b61e5ed2
EsmondKim <ekelectronicmail@gmail.com>
2020-11-22 18:57:13 -0600

    Fixed accidentally deleted semicolon at end of line 7, highscorescript.js.

--- highscorescript.js
+++ highscorescript.js
@@ -4,4 +4,4 @@ let lastFighterScoreEl = document.querySelector("#lastFighterScore");
 let lastFighterEl = document.querySelector("#lastFighter");
 
 lastFighterScoreEl.textContent = localStorage.getItem("lastFighterScore");
-lastFighterEl.textContent = localStorage.getItem("lastFighter")
+lastFighterEl.textContent = localStorage.getItem("lastFighter");
